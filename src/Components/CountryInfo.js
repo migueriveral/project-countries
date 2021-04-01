5ef2c78013902e1b4cc6b38a346b07bbae157864
Miguel Rivera <migue.rlopez@gmail.com>
2021-04-01 13:44:31 -0300

    better syntax and bug fix

--- src/Components/CountryInfo.js
+++ src/Components/CountryInfo.js
@@ -1,50 +1,63 @@
 import React, {useState, useEffect} from "react"
 
 function CountryInfo() {
-    const [loading, setLoading] = useState(true)
-    const [countryName, setCountryName] = useState("")
-    const [countryCapital, setCountryCapital] = useState("")
-    const [countryPopulation, setCountryPopulation] = useState(0)
-    const [countryRegion, setCountryRegion] = useState("")
-    const [countrySubRegion, setCountrySubRegion] = useState("")
-    const [countryArea, setCountryArea] = useState(0)
-    const [countryFlag, setCountryFlag] = useState("")
-    
+    const [loading, setLoading] = useState('idle')
+    const [error, setError] = useState('')
+
+    const [country, setCountry] = useState({
+        name: '',
+        capital: '',
+        population: 0,
+        region: '',
+        subregion: '',
+        area: 0,
+        flag: ''
+    })
 
     function fetchData() {
+        setLoading('pending')
         fetch("https://restcountries.eu/rest/v2/all")
             .then(response => response.json())
             .then(data => {
                 var randomCountry = Math.floor(Math.random() * (data.length - 1) + 1)
-                setLoading(false)
-                setCountryName(data[randomCountry].name)
-                setCountryCapital(data[randomCountry].capital)
-                setCountryPopulation(data[randomCountry].population)
-                setCountryArea(data[randomCountry].area)
-                setCountryRegion(data[randomCountry].region)
-                setCountrySubRegion(data[randomCountry].subregion)
-                setCountryFlag(data[randomCountry].flag)
+                setCountry(data[randomCountry])
+                setLoading('resolved')
             })
+            .catch((error) => {
+                setLoading('rejected')
+                setError(error)
+                })
     }
 
-    useEffect(() => {
+     useEffect(() => {
         fetchData()
-    }, [])
+    }, []) 
 
     return (
         <div className="country-info">
-            <div style={{display: loading? 'block' : 'none'}}><p>Loading...Please wait</p></div>
-            <div style={{display: loading? 'none' : 'block'}}>
-                <h1>Random Country: {countryName}</h1>
+            <div 
+                style={{display: loading === 'pending' || loading === 'idle'? 'block' : 'none'}
+            }>
+                <p>Loading...Please wait</p>
+            </div>
+            <div 
+                style={{display: loading === 'rejected' ? 'block' : 'none'}
+            }>
+                <p>Oops an error ocurred: </p> <pre>{error.message}</pre>
+            </div>
+            <div 
+                style={{display: loading === 'resolved' ? 'block' : 'none'}
+            }>
+                <h1>Random Country: {country.name}</h1>
                 <hr />
                 <p>Let's get some facts!</p>
-                <p>Capital: {countryCapital}</p>
-                <p>Population: {countryPopulation}</p>
-                <p>Region: {countryRegion}</p>
-                <p>Sub-region: {countrySubRegion}</p>
-                <p>Area: {countryArea} km</p>
+                <p>Capital: {country.capital}</p>
+                <p>Population: {country.population}</p>
+                <p>Region: {country.region}</p>
+                <p>Sub-region: {country.subRegion}</p>
+                <p>Area: {country.area} km</p>
             </div>
-            <img src={countryFlag}></img>
+            <img src={country.flag} alt='country flag'></img>
             <form onSubmit={fetchData}><button>New Country</button></form>
         </div>
     )
---
+++ src/Components/Favbar.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Favbar() {
+    
+}
+
+export default Favbar
